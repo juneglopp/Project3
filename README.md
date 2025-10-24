1b35114a7df1b7e3251d30d1d8bf41669b4c0d2a
Sofieforland <144887478+Sofieforland@users.noreply.github.com>
2025-10-24 15:27:02 +0200

    task 1

---
+++ README.md
@@ -0,0 +1,18 @@
+# Project3
+
+### Setup
+```bash
+
+# 1) Gå til prosjektet ditt
+cd /Users/sofieforland/Documents/5.semester/MOD300/project3/Project3
+
+# 2) Lag et lokalt venv med Homebrew-Python
+/opt/homebrew/bin/python3 -m venv .venv
+
+# 3) Aktiver venv
+source .venv/bin/activate
+
+# 4) Installer numpy i dette venv-et og kjør scriptet
+python -m pip install --upgrade pip
+python -m pip install numpy
+python functions.py
--- functions.py
+++ functions.py
@@ -1,3 +1,25 @@
 # Topic 1
 
-# Task 0:
+# Task 1:
+import numpy as np
+
+
+"""
+Generate one random (x, y, z) point inside the simulation box.
+    The point is drawn from a uniform distribution.
+    
+    Parameters:
+    box_min : array-like 3 values
+        Minimum coordinates [x_min, y_min, z_min] (in nm)
+    box_max : array-like 3 values
+        Maximum coordinates [x_max, y_max, z_max] (in nm)
+
+    Returns:
+    numpy.ndarray of shape 3
+    -Random point [x, y, z] inside the box
+"""
+
+def random_point_in_box(box_min, box_max):
+    point = np.random.uniform(box_min, box_max)
+    return point
+
